6ce03a42eb116e3d7fd2cbffe1fe59b8ba9b9b6d
Ligia Takayama <ligiantakayama@gmail.com>
2023-08-10 19:04:47 +0200

    validação com JavaScript

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,31 @@
-const form = document .getElementById("form-confirm");
+const form = document.getElementById("form-confirm");
 
 let formValid = false;
 
-function numberValid (ValA, ValB) {
+function numbervalid (ValA, ValB){
     return ValB > ValA;
 }
 
-form.addEventListener("submit", function(e) {
-preventDeFault (),
-    let ValA = parseFloat(document.getElementById("valor_a").value);
-    let ValB = parseFloat(document.getElementById("valor_b").value);
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    let ValA = parseFloat(document.getElementById("valor-a").value);
+    let ValB = parseFloat(document.getElementById("valor-b").value);
 
-    const messagesucess = "Dados enviados com sucesso" 
+    const messagesucess = "Formulário enviado com sucesso!";
     const containermessagesucess = document.querySelector(".success-message");
 
+    formValid = numbervalid(ValA, ValB);
 
-formValid = numberValid (ValA, ValB);
+    if (formValid) { 
+        document.querySelector(".error.message").style.display = "none";
+        containermessagesucess.innerHTML = messagesucess; 
+        containermessagesucess.style.display = "block";
+        document.querySelector(".error-message").style.display = "none";
 
-if (formValid) {
-    document.querySelector(".error-message").style.display = "none";
-    containermessagesucess.innerHTML = messagesucess;
-    containermessagesucess.style.display = "block";
-    document.querySelector(".error-message").style.display = "none";
-
-    ValA.value = " ";
-    ValB.value = " ";
-} Else {
-    document.querySelector(".error-message").style.display = "block";
-    containermessagesucess.style.display = "none";
-}
-
-
+        ValA.value = " ";
+        ValB.value = " ";
+    } else {
+        document.querySelector(".error-message").style.display = "block";
+        containermessagesucess.style.display = "none";
+    }
 })
