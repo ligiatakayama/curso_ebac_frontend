88ff2b89ec0772f90215795e6d6130cd26c10002
Ligia Takayama <ligiantakayama@gmail.com>
2023-09-06 16:18:56 +0200

    implemntação dos campos endereço, cep e cpf utilizando máscaras

--- lojaautomoveis/js/main.js
+++ lojaautomoveis/js/main.js
@@ -7,7 +7,17 @@ $(document).ready(function() {
         $('nav').slideToggle();
     })
 
-    $('#telefone').mask('(00) 00000-0000')
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder:'00000-000'
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder:'000.000.000-00'
+    })
 
     $('form').validate({
         rules: {
